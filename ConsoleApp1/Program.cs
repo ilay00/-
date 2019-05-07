ce720b3dcfb1af3c258e24763bc959b1c83d1a0d
ilay00 <agidelio@mail.ru>
2019-05-07 19:51:10 +0500

    rt

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+
+    class Program
+    {
+        class StaticClass
+        {
+           
+
+            public void arryVal(Array array, string vals)   //метод поиск значения дилимая на 3
+            {
+
+                int targets = int.Parse(vals);
+
+
+                int indexs = Array.BinarySearch(array, targets);
+                int c;
+
+                if (indexs >= 0)
+                {
+                    c = indexs % 3;
+
+
+
+                    if (c != 0)
+                    {
+                        Console.WriteLine("Мы нашли значения .Выберите его.");
+                        Console.WriteLine(c);
+                    }
+                    else
+                    {
+                        /* Console.WriteLine("Мы нашли значения .Выберите его.");
+                         Console.WriteLine(c);*/
+                        Console.WriteLine("Мы нашли значения .Выберите его.");
+                        Console.WriteLine(c);
+
+                    }
+
+                }
+
+                else
+                {
+                    indexs = -indexs;
+                    if (indexs >= targets) indexs = targets - 1;
+
+                    
+                    Console.WriteLine("не нашли значение Выберите ближайшее ");
+                    Console.Write(indexs);
+
+                    Console.ReadLine();
+                }
+            }
+
+
+
+
+            public StaticClass(int arraysiz, int rendcol)    //конструктор
+            {
+                int[] arrayss;
+                arrayss = new int[arraysiz];
+                Random rnd = new Random();
+                for (int i = 0; i < arrayss.Length; i++)
+                {
+                     arrayss[i] = rnd.Next(0, rendcol);
+                    
+                }
+
+            }
+
+
+           
+
+
+
+           
+
+
+        }
+
+        static void Main(string[] args)
+            {
+                int[] arrays;
+                arrays = new int[20];
+                /*  Random rnd = new Random();
+                  for (int i = 0; i < arrays.Length; i++)
+                  {
+                      arrays[i] = rnd.Next(0, 5);
+
+
+                  }*/
+
+
+
+
+                StaticClass C = new StaticClass(9, 9);
+                C.arryVal(arrays, "3");
+                C.arryVal(arrays, "6");
+               
+
+            }
+        }
+    }
+
+
+ 
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+
+
+
+
+        static public void arryVal(Array array, string vals)
+        {
+
+            int targets = int.Parse(vals);
+
+
+            int indexs = Array.BinarySearch(array, targets);
+            int c;
+
+            if (indexs >= 0)
+            {
+                c = indexs %3;
+
+
+
+                if (c!= 0)
+                {
+                   
+                }
+                else
+                {
+                   
+                    Console.WriteLine("Мы нашли значения.Выберите его.");
+                    Console.WriteLine(c);
+
+                }
+
+            }
+
+            else
+            {
+                indexs = -indexs;
+                if (indexs >= targets) indexs = targets - 1;
+                 
+                // Мы не нашли цель. Выберите ближайшее значение.
+                Console.WriteLine("Мы не нашли значение. Выберите ближайшее значение");
+                Console.Write(indexs);
+
+                Console.ReadLine();
+            }
+
+
+        }
+    
+    
+        
+
+        static void Main(string[] args)
+        {
+            int[] arrays;
+            arrays = new int[5];
+            Random rnd = new Random();
+            for (int i= 0; i < arrays.Length; i++)
+            {
+                arrays[i] = rnd.Next(0,5);
+
+               
+            }
+             var arval = "3";
+            arryVal(arrays, arval);
+             var arvals = "1";
+            arryVal(arrays, arvals);
+
+
+
+
+        }
+    }
+}
